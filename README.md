ed628111a4ab28b3a7e7447d6e11239b9dc577b3
dulee <duckyounglee@oden.kr>
2022-06-15 16:33:23 +0900

    test filepath

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Test for run Python in JS.
 
 It can also run exec
+
+file paht 는 명령어 실행 위치.
+
+npm은 당연히 package.json위치다.
--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@
 const spawn = require('child_process').spawn;
 
 // 2. spawn을 통해 "python 파이썬파일.py" 명령어 실행
-const result = spawn('python', ['py.py']);
+const result = spawn('python', ['python/py.py']);
 
 // 3. stdout의 'data'이벤트리스너로 실행결과를 받는다.
 result.stdout.on('data', function(data) {
